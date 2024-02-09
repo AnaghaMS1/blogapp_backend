88a0b440345c5497943b54df381cba2c52d703d1
AnaghaMS1 <anaghasalbu05@gmail.com>
2024-02-09 14:48:53 +0530

    update

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 
-
-
 const express=require("express")
 const cors=require("cors")
 const mongoose=require("mongoose")
  const userRouter=require("./controllers/userRouter")
+ const postRouter=require("./controllers/postRouter")
 
 const app=express()
 app.use(express.json())
 app.use(cors())
 
 app.use("/users",userRouter)
-
+app.use("/post",postRouter)
 
 mongoose.connect("mongodb+srv://Anagha123:anagha123@cluster0.8e1jiqb.mongodb.net/userDb?retryWrites=true&w=majority",
 {
