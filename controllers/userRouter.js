2b3979a448ca91e52d6cc0d79164a472a3d37610
AnaghaMS1 <anaghasalbu05@gmail.com>
2024-02-09 14:05:40 +0530

    update

--- controllers/userRouter.js
+++ controllers/userRouter.js
@@ -13,7 +13,7 @@ hashPasswardGenertor=async (pass)=>{
 router.post("/signup",async(req,res)=>{
     let {data}={"data":req.body}
     let password=data.password
-    hashPasswardGenertor(password).then(
+    /*hashPasswardGenertor(password).then(
         (hashedPassword)=>{
             console.log(hashedPassword)
             data.password=hashedPassword
@@ -26,6 +26,57 @@ router.post("/signup",async(req,res)=>{
         }
     )
     
+})
+
+router.post("/signin",async(req,res)=>{
+    let input =req.body
+    let data=await usermodels.find(input)
+    res.json({
+
+          status:"success"
+    })*/
+const hashedPassword=await hashPasswardGenertor(password)
+data.password=hashedPassword
+let user=new usermodels(data)
+let result=await user.save(data)
+res.json({
+
+    status:"success"
+})
+})
+
+router.post("/signin",async(req,res)=>{
+    let input =req.body
+    let emailid=req.body.emailid
+//reetrive all info of emailid
+
+    let data=await usermodels.findOne({"emailid":emailid})
+    if(!data)
+    {
+        return res.json({
+
+            status:"invalid user"
+        })
+    }
+    console.log(data)
+    let dbPassword=data.password
+    let inputPassword=req.body.password
+    console.log(dbPassword)
+    console.log(inputPassword)
+    const match=await bcrypt.compare(inputPassword,dbPassword)
+    if(!match){
+        return res.json ({
+
+            status:"invalid password"
+        })
+    }
+    res.json({
+
+        status:"success"
+    })
+
+
+
 })
 
 
